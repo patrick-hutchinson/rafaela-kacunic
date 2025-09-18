46721a28368108370570c8260b1e12f7b63eb0ae
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2025-09-18 11:54:07 +0200

    Update page.jsx

--- src/app/(page)/page.jsx
+++ src/app/(page)/page.jsx
@@ -2,7 +2,7 @@ import { getProjects, getHome, getAbout } from "@/sanity/lib/api";
 
 import Home from "./HomeClient";
 
-export const revalidate = 10; // seconds
+// export const revalidate = 10; // seconds
 
 export default async function Page() {
   const [projects, home, about] = await Promise.all([getProjects(), getHome(), getAbout()]);
